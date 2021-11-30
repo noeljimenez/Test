08df6acf3e20dcbb55944ef76bdbc718378f4c59
Ferran Llobet <ferran.llobet@epiagranollers.cat>
2021-11-30 17:44:19 +0100

    Close #11 Pintar de blanc
    Close #13 Pintar de vermell

--- Pràctica GitHub/Pràctica GitHub/main.cpp
+++ Pràctica GitHub/Pràctica GitHub/main.cpp
@@ -5,22 +5,39 @@
 #include <string>
 #include <iostream>
 
+void PintaDeVermell(SDL_Surface* surface);
+void Pinta10quadrats(SDL_Surface* surface);
+
 int main() {
 
 	// TODO: Creació de finestra
+	
 
-	// TODO: Finestra blanca al iniciar
+	// DONE: Finestra blanca al iniciar
+	SDL_FillRect(windowSurface, NULL, SDL_MapRGB(windowSurface->format, 255, 255, 255));
 
 	// TODO: Captura de tecles: 1,2 i ESC
 
-	// TODO: Funció que pinta de vermell la finestra
+
+	// TODO: Funció que pinta de vermell la finestra XXXXXXXXX
+	PintaDeVermell(windowSurface);
+
 
 	// TODO: Funció que pinta un quadrat aleatori verd
 
-	// TODO: Funció que pinta els 10 quadrats
+
+	// TODO: Funció que pinta els 10 quadrats XXXXXXXXX
+	
+
 
 	// TODO: Fer que el programa surti amb ESC
 
 
 	return 0;
 }
+
+void PintaDeVermell(SDL_Surface* surface) {
+	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 255, 0, 0));
+}
+
+
